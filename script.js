0cb69e9c1fa4a2bc7101358f4eaac40d121ba723
Chelsea Runacres <chelsea.runacres@gmail.com>
2022-11-15 13:26:47 -0500

    questions structure and content added

---
+++ script.js
@@ -0,0 +1,17 @@
+var questions = [
+    {ask: "Horses can rotate their ears 180 degrees and move them independantly from one another. How many muscles doe horses have in their ears?",
+     answer: ["7", "15", "32", "10"],
+     correct: "10"}
+    {ask: "On average, horse hearts weigh between 4kg and 4.5kg, and are about the same size as which of the following:",
+     answer: ["A grapefruit", "A basketball", "A tennis ball", "A  microwave"],
+     correct: "A basketball"}
+    {ask: "The worlds smallest horse breed is a Falabella, which ranges from 15in to 30in in height. The tallest horse on record, Sampson stood at which measurement?",
+     answer: ["5 feet 6.5in", "8 feet 2in", "7 feet 2.5in", "10 feet 0.5in"],
+     correct: "7 feet 2.5in"}
+     {ask: "All existing domestic horse breeds can fit into 5 categories. Which of the following is NOT one of those five?",
+     answer: ["Warm-blooded", ""],
+     correct: ""}
+     {ask: "",
+     answer: [],
+     correct: ""}
+]
